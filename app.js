416dd25e977757a2f6c555be5c80e4d9fa6104be
Xhemajl Camaj <Xhemajlc@Agrons-MacBook-Pro-2.local>
2022-02-21 12:54:47 -0500

    html file generates with user and github

--- app.js
+++ app.js
@@ -1,18 +1,29 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+const fs = require('fs')
+const generatePage = require('./src/page-template.js')
+
+const profileDataArgs = process.argv.slice(2);
+
+const [user, github] = profileDataArgs
+
+fs.writeFile('index.html', generatePage(user, github), err =>{
+     if (err) throw err;
+
+     console.log("Portfolio complete! Check out index html to see the output!")
+ })
 
 
-const printProfileData = profileDataArr => {
-    for (let i = 0; i < profileDataArr.length; i++) {
-        console.log(profileDataArr[i]);
-    }
+// const printProfileData = profileDataArr => {
+//     for (let i = 0; i < profileDataArr.length; i++) {
+//         console.log(profileDataArr[i]);
+//     }
 
 
-console.log('================');
+// console.log('================');
 
-// Is the same as this...
-profileDataArr.forEach(profileItem =>  console.log(profileItem));
-}
+// // Is the same as this...
+// profileDataArr.forEach(profileItem =>  console.log(profileItem));
+// }
 
 
-printProfileData(profileDataArgs)
+// printProfileData(profileDataArgs)
 
