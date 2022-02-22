e9f898853f01349ad294a5873d8bbcf887b6a92d
Xhemajl Camaj <Xhemajlc@Agrons-MacBook-Pro-2.local>
2022-02-22 13:11:53 -0500

    updated html template

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const fs = require('fs')
+const generatePage = require('./src/page-template.js')
 const inquirer = require('inquirer');
 
 const promptUser = () => {
@@ -29,11 +31,6 @@ const promptUser = () => {
       }
     },
     {
-      type: 'input',
-      name: 'about',
-      message: 'Provide some information about yourself:'
-    },
-    {
       type: 'confirm',
       name: 'confirmAbout',
       message: 'Would you like to enter some information about yourself for an "About" section?',
@@ -43,27 +40,24 @@ const promptUser = () => {
       type: 'input',
       name: 'about',
       message: 'Provide some information about yourself:',
-      when: ({ confirmAbout }) => {
-        if (confirmAbout) {
-          return true;
-        } else {
-          return false;
-        }
-      }
+      when: ({ confirmAbout }) => confirmAbout
     }
-  ])
-}
+  ]);
+};
 
 const promptProject = portfolioData => {
-  if (!portfolioData.projects) {
-    portfolioData.projects = [];
-  }
+
   console.log(`
 =================
 Add a New Project
 =================
 `);
-  return inquirer.prompt([
+
+  if (!portfolioData.projects) {
+    portfolioData.projects = [];
+  }
+  return inquirer
+  .prompt([
     {
       type: 'input',
       name: 'name',
@@ -136,11 +130,15 @@ Add a New Project
 promptUser()
   .then(promptProject)
   .then(portfolioData => {
-    console.log(portfolioData);
+    const pageHTML = generatePage(portfolioData);
+
+     fs.writeFile('./index.html', pageHTML, err => {
+      if (err) throw new Error(err);
+
+       console.log('Page created! Check out index.html in this directory to see it!');
+     });
   });
 
-// const fs = require('fs')
-// const generatePage = require('./src/page-template.js')
 
 // const [user, github] = profileDataArgs
 
